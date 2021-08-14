7211c41ba651850f4d76197a59f159627a8a4953
My Phan <myphan@cs.umass.edu>
2021-08-13 23:01:04 -0400

    Adding Gurobi solver for function T = l2 norm.

--- bound_LP_solver.py
+++ bound_LP_solver.py
@@ -6,15 +6,17 @@ from helper import induced_mean
 
 
 
-def b_alpha_gurobi(z, alpha,  T , lower = 0, upper = 1, num_samples = 10000):
+def b_alpha_gurobi(z, alpha,  T = None, l2norm = False, lower = 0, upper = 1, num_samples = 10000):
     """
     Args:
       z: The samlpe
       alpha: confidence parameter
       T: the coefficient of the linear function mapping the sample z to a real number
+      l2norm: if l2norm is true, the function mapping the sample z to a real number is the l2 norm of z.
       lower: lower bound of the support
       upper: upper bound of the support
       num_samples: the number of Monte Carlo samples
+
     Examples:
 
       When the functino is the sample mean, T is a vector of 1.
@@ -29,10 +31,8 @@ def b_alpha_gurobi(z, alpha,  T , lower = 0, upper = 1, num_samples = 10000):
     u_list = np.random.rand(num_samples, n)
 
     sorted_u_list = np.sort(u_list, axis = 1)
-    n = len(z)
     sorted_u_list0 = np.append(np.zeros((num_samples, 1)), sorted_u_list, axis = 1)
     neg_u_delta_list = sorted_u_list0[:, :n] - sorted_u_list0[:, 1:]
-    T = np.array(T)
 
 
     model = gp.Model()
@@ -40,7 +40,12 @@ def b_alpha_gurobi(z, alpha,  T , lower = 0, upper = 1, num_samples = 10000):
     # Constant term of the function f(x). This is a free continuous variable that can take positive and negative values.
     x = model.addMVar(n, lb=lower , ub=upper, vtype=GRB.CONTINUOUS, name="x")
     order = model.addConstrs( (x[i] - x[i+1] <= 0 for i in range(n-1)), name='order')
-    region = model.addConstr( (x @ T - z @ T <= 0) , name='region')
+    if not l2norm:
+        assert T is not None
+        T = np.array(T)
+        region = model.addConstr( (x @ T - z @ T <= 0) , name='region')
+    else:
+        region = model.addQConstr( (x @ x - z @ z <= 0) , name='region')
 
 
     for j in range(len(u_list)):
@@ -57,7 +62,7 @@ def b_alpha_gurobi(z, alpha,  T , lower = 0, upper = 1, num_samples = 10000):
             m_opt = induced_mean(x_opt,u, upper = upper)
             b_list.append(m_opt)
         else:
-            print("GUROBI does not return optimal solution. Status: " + model.status)
+            print("GUROBI does not return optimal solution. Status: " + str(model.status))
             break
 
 
--- run.py
+++ run.py
@@ -16,11 +16,16 @@ for i in range(10):
     print("Sample size:",n , ", alpha:", alpha)
     print("Sample:", z)
     print("New bound with T = Anderson (Gurobi solver) and  D = [-inf,1]:", b_alpha_gurobi(z, alpha, T, lower = -np.inf))
-    print("New bound with T = Anderson (manual solver) and  D = [-inf,1]:",b_alpha_linear(z, alpha, T, lower = -np.inf))
+    print("New bound with T = Anderson (manual solver) and  D = [-inf,1]:",b_alpha_linear(z, alpha, T, lower = -np.inf), "\n")
+
     print("New bound with T = Anderson (Gurobi solver) and  D = [0,1]:", b_alpha_gurobi(z, alpha, T, lower = 0))
-    print("New bound with T = Anderson (manual solver) and  D = [0,1]:",b_alpha_linear(z, alpha, T, lower = 0))
-    print("Anderson's bound when the upper bound of the support is 1: ", anderson(z, alpha))
-    print("New bound with T = l2 norm with D = [0,1]: ", b_alpha_l2norm(z,  alpha), "\n")
+    print("New bound with T = Anderson (manual solver) and  D = [0,1]:",b_alpha_linear(z, alpha, T, lower = 0), "\n")
+
+    print("Anderson's bound when the upper bound of the support is 1: ", anderson(z, alpha), "\n")
+
+    print("New bound with T = l2 norm (Gurobi solver) with D = [0,1]: ", b_alpha_gurobi(z,  alpha, l2norm = True))
+    print("New bound with T = l2 norm (manual solver) with D = [0,1]: ", b_alpha_l2norm(z,  alpha), "\n")
+
 
 
 
